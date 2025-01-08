6c8978c0b28626e9e6578307373b84352d5995fc
GabrielMM182 <gmm_morais@hotmail.com>
2025-01-07 22:55:09 -0300

    initial config

---
+++ src/main/java/com/projetoBtgPag/projetoBtg/ProjetoBtgApplication.java
@@ -0,0 +1,13 @@
+package com.projetoBtgPag.projetoBtg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBtgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBtgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoBtgPag/projetoBtg/ProjetoBtgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoBtgPag.projetoBtg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBtgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
